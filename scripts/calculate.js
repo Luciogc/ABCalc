7b170ddeb8a2645184371c2e7df619e3253f9471
Lucio C <75331530+Luciogc@users.noreply.github.com>
2021-08-28 20:31:32 -0700

    Adding new and removing unnecessary features.

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -1,4 +1,5 @@
 var boardList = [];
+var prices = [];
 
 function calculate() {
   var length = document.getElementById('length').value;
@@ -6,16 +7,15 @@ function calculate() {
   var thickness = document.getElementById('thickness').value;
 
   var price = document.getElementById('price').value;
-  var rounding = document.getElementById('rounding').value;
 
   var boardfeet = (length * width * thickness) / 144;
-  boardfeet = boardfeet.toFixed(rounding);
+  boardfeet = boardfeet.toFixed(2);
 
   boardList.push(parseFloat(boardfeet));
   document.getElementById('boardlist').value = boardList;
 
   var tboardfeet = boardList.reduce((a, b) => a + b, 0);
-  tboardfeet = tboardfeet.toFixed(rounding);
+  tboardfeet = tboardfeet.toFixed(2);
   document.getElementById('tboardfeet').value = tboardfeet;
   document.getElementById('tboards').value = boardList.length;
 
@@ -23,21 +23,28 @@ function calculate() {
 
   if (price.length !== 0) {
     var pricecalc = boardfeet * price;
-    pricecalc = pricecalc.toFixed(rounding);
+    pricecalc = pricecalc.toFixed(2);
+
+    prices.push(parseFloat(pricecalc));
+    var tprice = prices.reduce((a, b) => a + b, 0);
+    tprice = tprice.toFixed(2);
+
+    document.getElementById('tprice').value = tprice;
     document.getElementById('pricecalc').value = pricecalc;
   }
 }
 
 function clear() {
   boardList = [];
+  prices = [];
   document.getElementById('length').value = "";
   document.getElementById('width').value = "";
   document.getElementById('thickness').value = "";
   document.getElementById('price').value = "";
-  document.getElementById('rounding').value = "2";
   document.getElementById('boardlist').value = "";
   document.getElementById('tboardfeet').value = "";
   document.getElementById('tboards').value = "";
   document.getElementById('boardfeet').value = "";
+  document.getElementById('tprice').value = "";
   document.getElementById('pricecalc').value = "";
 }
